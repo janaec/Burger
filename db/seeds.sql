4aaf6708be69b519a7bbd8ecf3ede92c162d24ca
email <jjazminclrk@gmail.com>
2020-12-17 23:23:12 -0500

    adding seeds file and three seeds

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hennessy Burger', false);
