135d4cac843281a7966ba313638d8be791a90d6a
email <jjazminclrk@gmail.com>
2020-12-19 01:09:18 -0500

    updating the controller file

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,60 @@
+var express = require("express");
+
+var router = express.Router();
+
+
+var burger = require("../models/burgers.js");
+
+router.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+
+router.get("/burger", function(req, res) {
+    burger.all(function(data) {
+    res.json({ burgers: data });
+  });
+});
+
+router.post("/create", function(req, res) {
+  burger.create([
+    "burger_name"
+  ], [
+    req.body.burger_name
+  ], function(result) {
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put("/devour/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  console.log("condition", condition);
+
+  burger.update({
+    devoured:true
+  }, condition, function(result) {
+    if (result.changedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.json({ id: req.params.id });
+    }
+  });
+});
+
+router.delete("/delete/:id", function(req, res) {
+  var condition = "id = " + req.params.id;
+
+  burger.delete(condition, function(result) {
+    if (result.affectedRows == 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    } else {
+      res.status(200).end();
+    }
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
